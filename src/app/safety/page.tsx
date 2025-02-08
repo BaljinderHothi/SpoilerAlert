80b4a6bcc3fd14dfbfa3ec855727c5765ce3f1bb
IshmamF <ishmamf2003@gmail.com>
2025-02-08 13:42:02 -0500

    navbar, responsive and color themes from pallet

---
+++ src/app/safety/page.tsx
@@ -0,0 +1,5 @@
+export default function Safety() {
+    return (
+        <div>Check safety of a food</div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Qwitcher: ["Qwitcher Grypen", "cursive"]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
