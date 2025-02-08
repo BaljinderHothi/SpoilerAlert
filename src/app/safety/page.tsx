c77dca45803c88566fbba4fd754f395ac3b3f879
IshmamF <ishmamf2003@gmail.com>
2025-02-08 18:23:50 -0500

    imported shadcn and fancy components and put them together for tab functionality

--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -1,18 +1,19 @@
 'use client'
-import Camera from "./_components/camera";
 import { useState } from "react";
+import Test from "./_components/test";
 
 export default function Safety() {
     const [detectedObject, setDetectedObject] = useState<string>("");
 
     return (
         <>
-            <Camera setObject={setDetectedObject} />
+            {/*<Camera setObject={setDetectedObject} />
 
             <div className="mt-4 w-96 bg-white p-4 rounded-lg shadow-md">
                 <h2 className="text-lg font-semibold text-gray-700">Detected Object:</h2>
                 <p className="text-gray-600 mt-2">{detectedObject || "No object detected"}</p>
-            </div>
+            </div> */}
+            <Test/>
         </>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,30 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        Qwitcher: ["Qwitcher Grypen", "cursive"]
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			Qwitcher: [
+  				'Qwitcher Grypen',
+  				'cursive'
+  			]
+  		},
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
