69452f0eb175bf5871faabb30a9adb94e0afb7de
jaynopponep <noppone.p@gmail.com>
2025-02-08 15:58:17 -0500

    new configs

--- backend/train.py
+++ backend/train.py
@@ -7,16 +7,22 @@ load_dotenv()
 api_key = os.getenv('ROBOFLOW_API_KEY')
 rf = Roboflow(api_key)
 project = rf.workspace("mldatasets-aiiqt").project("spoiler-alert-cloned-dataset-1ryna")
-version = project.version(1)
+version = project.version(2)
 dataset = version.download("yolov8")
 
 def main():
     torch.cuda.empty_cache()
+    print(torch.version.cuda)
+    print(torch.cuda.is_available())
+    print(os.environ.get('CUDA_VISIBLE_DEVICES'))
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     model = YOLO("yolov8n.pt")
+    train_loader = DataLoader(dataset, batch_size=16, num_workers=0)
 
     train_results = model.train(
         data=f"{dataset.location}/data.yaml",
-        epochs=200,
+        epochs=40,
         imgsz=512,
         batch=2,
         device='cuda',
