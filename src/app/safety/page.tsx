dcd33f28f469287e20a3fd33abc90cf4b9d53144
IshmamF <ishmamf2003@gmail.com>
2025-02-08 15:55:39 -0500

    camera component

--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -1,5 +1,11 @@
+'use client'
+import WebcamStreamCapture from "./_components/camera";
+
 export default function Safety() {
     return (
-        <div>Check safety of a food</div>
+        <>
+            <div>Check safety of a food</div>
+            <WebcamStreamCapture/>
+        </>
     )
 }
