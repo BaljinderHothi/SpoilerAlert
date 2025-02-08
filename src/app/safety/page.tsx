33daf2ed68aecb58f78cbeca1667964d3aca0858
IshmamF <ishmamf2003@gmail.com>
2025-02-08 18:40:04 -0500

    fixed some issues that occured after merge

--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -18,18 +18,5 @@ export default function Safety() {
             </div> */}
             <Test/>
         </>
-        {/*
-        <div className="flex space-x-4">
-            <div className="flex-1">
-                <Camera setObject={setDetectedObject} />
-            </div>
-            <div className="flex-1">
-                <Sidebar />
-            </div>
-            <div className="mt-4 w-96 bg-white p-4 rounded-lg shadow-md">
-                <h2 className="text-lg font-semibold text-gray-700">Detected Object:</h2>
-                <p className="text-gray-600 mt-2">{detectedObject || "No object detected"}</p>
-            </div>
-        </div> */}
     );
 }
