aa48b5f9c4a7adba1a5695ee00a375bc47e7baba
IshmamF <ishmamf2003@gmail.com>
2025-02-09 01:21:34 -0500

    api call to get currnet position and city/state/suburb data

--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -1,21 +1,47 @@
 // pages/safety.tsx
 'use client'
-import Camera from "./_components/camera";
-import Sidebar from "./_components/sidebar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Test from "./_components/test";
 
 export default function Safety() {
     const [detectedObject, setDetectedObject] = useState<string>("");
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+    const [data, setData] = useState({})
+    const ACCESS_TOKEN = "pk.38580c3f69a72023f4dd81db9cc4cb9a"
+
+    function success(position:any) {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        setLatitude(lat)
+        setLongitude(long)
+
+    }
+
+    function error() {
+        console.log("Unable to retrieve your location");
+    }
+
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(success, error);
+        async function getUserLocationData() {
+            if (latitude && longitude) {
+                const api = `https://us1.locationiq.com/v1/reverse?format=json&key=${ACCESS_TOKEN}&lat=${latitude}&lon=${longitude}`
+                const options = {method: 'GET', headers: {accept: 'application/json'}};
+                const response = await fetch(api, options)
+                const result = await response.json();
+                setData(result);
+                console.log(result)
+            }
+        }
+        getUserLocationData()
+    }, [latitude, longitude]);
+
+    navigator.geolocation.getCurrentPosition(success, error);
 
     return (
         <>
-            {/*<Camera setObject={setDetectedObject} />
-
-            <div className="mt-4 w-96 bg-white p-4 rounded-lg shadow-md">
-                <h2 className="text-lg font-semibold text-gray-700">Detected Object:</h2>
-                <p className="text-gray-600 mt-2">{detectedObject || "No object detected"}</p>
-            </div> */}
             <Test/>
         </>
     );
